735a486771d8a276358c4c4bf91665c21bb0dd9a
011Jorge <vicentejorge187@gmail.com>
2022-12-22 14:30:29 -0300

    :construction: home page created

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../../components/navbar/navbar";
+
+import { ContainerHome } from "./homeStyles";
+
+function Home() {
+  return (
+    <ContainerHome>
+      <Nav />
+    </ContainerHome>
+  );
+}
+
+export default Home;
