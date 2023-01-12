17105ebc0a400e7115e6e0bbfdd12e63c690513a
011Jorge <vicentejorge187@gmail.com>
2023-01-11 22:35:43 -0300

    :lipstick: feat: adding height and padding

--- src/styles/appStyles.js
+++ src/styles/appStyles.js
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    
-    background-color: #202329;
-`
+  width: 100%;
+  height: auto;
+
+  padding-bottom: 80px;
+
+  background-color: #202329;
+`;
