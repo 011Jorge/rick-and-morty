2289019be2f781dd02d4c437e2ed5d64ac680538
011Jorge <vicentejorge187@gmail.com>
2023-01-11 22:40:36 -0300

    :lipstick: feat: updating the navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,25 +2,13 @@ import React from "react";
 
 import ImageLogo from "../../images/logo.png";
 
-import { ContainerNavbar, Image, Input, Navbar, Link } from "./navbarStyles";
+import { ContainerNavbar, Image, Input } from "./navbarStyles";
 
 function Nav() {
   return (
     <ContainerNavbar>
       <Image src={ImageLogo} alt="logo-rick-and-morty" />
       <Input type="text" placeholder="Buscar..." />
-
-      <Navbar className="">
-        <Link>
-          <a href="#">Character</a>
-        </Link>
-        <Link>
-          <a href="#">Location</a>
-        </Link>
-        <Link>
-          <a href="#">Episode</a>
-        </Link>
-      </Navbar>
     </ContainerNavbar>
   );
 }
