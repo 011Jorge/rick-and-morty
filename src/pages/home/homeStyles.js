e8734b192e6d2f0d61370aa2beff454cfd1931ed
011Jorge <vicentejorge187@gmail.com>
2023-01-13 19:38:10 -0300

    :iphone: header responsiveness

--- src/pages/home/homeStyles.js
+++ src/pages/home/homeStyles.js
@@ -11,15 +11,69 @@ export const Header = styled.header`
   align-items: center;
   text-align: center;
 
+  padding-top: 280px;
+
+  @media (max-width: 1400px) {
+    padding-top: 220px;
+  }
+
+  img {
+    @media (max-width: 450px) {
+      width: 200px !important;
+    }
+
+    @media (max-width: 650px) {
+      width: 350px !important;
+    }
+
+    @media (max-width: 1400px) {
+      width: 300px !important;
+    }
+  }
+
   h1 {
     font-size: 60px;
     font-family: "Goldman", cursive;
+
+    @media (max-width: 450px) {
+      width: 220px;
+      margin: 0 auto;
+    }
+
+    //ipad responsiveness
+    @media (max-width: 900px) {
+      font-size: 30px;
+    }
+
+    //monitor's responsiveness
+    @media (max-width: 1400px) {
+      font-size: 35px;
+    }
   }
 
   p {
     font-size: 23px;
     font-weight: 400;
     font-family: "Roboto", sans-serif;
+
+    //ipad responsiveness
+    @media (max-width: 900px) {
+      margin: 0 auto !important;
+      padding-top: 5px;
+
+      font-size: 18px;
+      width: 400px;
+    }
+
+    @media (max-width: 450px) {
+      font-size: 13px;
+      width: 220px;
+      padding-top: 10px;
+    }
+
+    @media (max-width: 1400px) {
+      font-size: 16px;
+    }
   }
 `;
 
@@ -37,7 +91,7 @@ export const Section = styled.section`
   border-radius: 12px;
 
   //iPhoneX responsiveness
-  @media (max-width: 560px) {
+  @media (max-width: 700px) {
     margin: 60px 0;
   }
 `;
