2f59f0ce37594a686b1aaea979d8f07caac35898
011Jorge <vicentejorge187@gmail.com>
2023-01-13 17:52:25 -0300

    :lipstick: header styling

--- src/pages/home/homeStyles.js
+++ src/pages/home/homeStyles.js
@@ -2,7 +2,28 @@ import styled from "styled-components";
 
 export const ContainerHome = styled.div``;
 
-export const Section = styled.div`
+export const Header = styled.header`
+  color: #fff;
+  padding-bottom: 20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+
+  h1 {
+    font-size: 60px;
+    font-family: "Goldman", cursive;
+  }
+
+  p {
+    font-size: 23px;
+    font-weight: 400;
+    font-family: "Roboto", sans-serif;
+  }
+`;
+
+export const Section = styled.section`
   margin: 60px;
   height: auto;
 
