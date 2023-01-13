9131d0d783093530c26cd8be9a38beb4ef55d8af
011Jorge <vicentejorge187@gmail.com>
2023-01-13 17:51:24 -0300

    :construction: adding a header to the website

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import CardCharacters from "../../components/characters/characters";
 
+import CardCharacters from "../../components/characters/characters";
 import Nav from "../../components/navbar/navbar";
 
-import { ContainerHome, Section } from "./homeStyles";
+import Image from "../../images/image.png";
+
+import { ContainerHome, Header, Section } from "./homeStyles";
 
 function Home() {
   return (
     <ContainerHome>
       <Nav />
+      <Header>
+        <img src={Image} alt="image-rick-and-morty" style={{ width: 500 }} />
+        <div>
+          <h1>Aventure-se descobrindo</h1>
+          <p>
+            informações sobre todos os personagens do incrível universo maluco
+            de rick and morty.
+          </p>
+        </div>
+      </Header>
       <Section>
         <CardCharacters />
       </Section>
